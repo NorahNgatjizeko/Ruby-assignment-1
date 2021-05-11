1e2bc0b9e46245060a620cf15c2e2f2b8f63812a
NorahNgatjizeko <81903330+NorahNgatjizeko@users.noreply.github.com>
2021-05-11 18:52:40 +0900

    Update janken.rb

--- janken.rb
+++ janken.rb
@@ -4,7 +4,6 @@ class Player
     puts "0:Rock, 1:Paper, 2:Scissors"
        input_hand = gets.to_i
      while true
-       input_hand = gets.chomp
          if input_hand == 0
            return 0
          elsif input_hand == 1
